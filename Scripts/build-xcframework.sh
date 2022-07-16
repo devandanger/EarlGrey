6700801d2728f3412e8c54034782b9e2cbf2d7ad
Evan Anger <devandanger@gmail.com>
2022-07-16 02:56:12 -0400

    Fixed framework and gitignore

--- Scripts/build-xcframework.sh
+++ Scripts/build-xcframework.sh
@@ -1,3 +1,3 @@
 xcodebuild archive -scheme EarlGrey -archivePath ./build/iphonesimulator.xcarchive -sdk iphonesimulator SKIP_INSTALL=NO
-xcodebuild archive -scheme EarlGrey -archivePath ~/build/iphoneos.xcarchive -sdk iphoneos SKIP_INSTALL=NO
+xcodebuild archive -scheme EarlGrey -archivePath ./build/iphoneos.xcarchive -sdk iphoneos SKIP_INSTALL=NO
 xcodebuild -create-xcframework -framework ./build/iphoneos.xcarchive/Products/Library/Frameworks/EarlGrey.framework -framework ./build/iphonesimulator.xcarchive/Products/Library/Frameworks/EarlGrey.framework -output ./build/EarlGrey.xcframework
